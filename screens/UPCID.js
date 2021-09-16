f5a719e6f90474b0dede9a007b67b5c63b3f3d57
amfaller <49420290+amfaller@users.noreply.github.com>
2021-09-15 23:40:23 -0400

    Issue with database not allowing new reads/writes

--- screens/UPCID.js
+++ screens/UPCID.js
@@ -6,14 +6,15 @@ import fonts from '../styles/fonts'
 import containers from "../styles/containers.js";
 
 import database from '@react-native-firebase/database';
+import firestore from '@react-native-firebase/firestore'
 
 // Put your FDA API key here
-var api_key = ''
+var api_key = 'VoN3IN2HARcFIO6lqLJMnI0UUY0SnU92P4xWBmpO'
 
 export default function UPCID() {
 
   // For general use with recipes
-  const [recipeNumber, setRecipeNumber] = useState(0);
+  const [recipeNumber, setRecipeNumber] = useState(1);
   
   // For use with UPC ID text input
   const [text, onChangeText] = useState('');
@@ -22,19 +23,23 @@ export default function UPCID() {
   // For use with queryFdaApi. Holds the returned JSON string
   var fdaOutput = '';
 
+  // ------------------------------------------------------------------
+  // This region is for FDA API querying and db writing
+
   // Function to create a new recipe in the Firebase db
   // TODO: Update this reference to be per-user
   function createRecipeInDb(recipeNumber) {
     const reference = database().ref('/recipes/' + recipeNumber);
+    console.log("Created a database reference entry at /recipes/" + recipeNumber);
   }
 
   // Function to save an FDA API query to the Firebase db
   // TODO: Update this reference to be per-user
   function sendJsonToDb(recipeNumber, upcID) {
-    database()
+    return database()
     .ref('/recipes/' + recipeNumber + '/' + upcID)
-    .set(fdaOutput)
-    .then(() => console.log('Data set.'));
+    .set(JSON.parse(fdaOutput));
+    
   }
 
   function getData(upcID) {
@@ -51,13 +56,13 @@ export default function UPCID() {
   // Function to query FDA API
   function queryFdaApi(recipeNumber, upcID) {
     // GET request
-    if(upcID.length == 12){
+    if(upcID && upcID.length == 12){
       getData(upcID)
       .then((response) => response.json())
       .then((responseJson) => {
         // On success, set fdaOutput to the JSON string
         fdaOutput = JSON.stringify(responseJson);
-        console.log("Response received: " + JSON.stringify(responseJson));   
+        // console.log("Response received: " + JSON.stringify(responseJson));   
       })
       .then(() => {
         // If no error occurs, send the JSON string to the database
@@ -68,7 +73,12 @@ export default function UPCID() {
         else{
           console.log("Sending object to db");
           sendJsonToDb(recipeNumber, upcID)
+            .then(() => console.log('Data set.'));
         }
+      })
+      // This .then block for testing database read
+      .then( () => {
+        getRecipe(0);
       })
       .catch((error) => {
         // Log any errors
@@ -79,9 +89,58 @@ export default function UPCID() {
     }
     else{
       console.log('Please enter a 12-digit UPC ID');
-    }
-    
+    } 
   }
+  // ------------------------------------------------------------------
+  // This region is for database reading
+
+  var initialized = 0;
+
+  // Function to get all ingredients of recipe recipeNumber
+  function getRecipe(recipeNumber){
+    console.log("In getRecipe");
+    if(initialized == 0){
+      // createRecipeInDb(0);
+      initialized = 1;
+    }
+
+    // For testing
+    // console.log("Creating test data...");
+    // const reference = database().ref('/users/test');
+    // database()
+    //   .ref('/users/test')
+    //   .set({
+    //     name: 'Test',
+    //     age: '21',
+    //   })
+    //   .then(() => console.log('Test data set.'))
+    //   .then(() => {
+    //     database()
+    //       .ref('/users/test')
+    //       .once('value')
+    //       .then(snapshot => {
+    //         console.log('Test data: ', snapshot.val());
+    //       })
+    //   });
+
+    database()
+      .ref('/recipes/' + recipeNumber)// + '/602652184024')
+      // .child('016000275690')
+      //.orderByValue()602
+      .once('value')   // 016000275690 for debugging
+        .then(snapshot => {
+          console.log("Database read: ");
+          console.log(snapshot.val());
+        })
+    var recipeArray = database().ref('/recipes/' + recipeNumber).orderByValue().once('value');
+  }
+
+
+  // ------------------------------------------------------------------
+  // This region for displaying data
+
+
+  // ------------------------------------------------------------------
 
   return (
     <View style={{ flex: 1, alignItems: 'center' }}>
@@ -127,6 +186,7 @@ export default function UPCID() {
               </Text>
             </Pressable>
           </SoftButton>
+          {getRecipe(1)}
         </View>
       </ScrollView>
     </View>
